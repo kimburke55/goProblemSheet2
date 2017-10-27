1ed00829b2f5a2adad3248529e401879e35720d1
kimburke55 <kimburke55@gmail.com>
2017-10-27 22:10:04 +0100

    h1 Header

--- h1.go
+++ h1.go
@@ -6,15 +6,16 @@ package main
 import (
 	"fmt"
 	"net/http"
-	
 )
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
+	//h1 header printing guessing game
 	fmt.Fprintln(w, "<h1>guessing game!</h1>")
 	
 }
 
 func main() {
 	http.HandleFunc("/", requestHandler)
+	//sends the information to port 8080
 	http.ListenAndServe(":8080", nil)
 }
